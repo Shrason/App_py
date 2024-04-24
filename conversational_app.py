92b2b137131c4e800b449e9bd2720f5a1069a128
Shrason <152863188+Shrason@users.noreply.github.com>
2024-04-24 16:14:03 +0530

    Update conversational_app.py

--- conversational_app.py
+++ conversational_app.py
@@ -7,9 +7,9 @@ key = f.read()
 genai.configure(api_key=key)
 
 model = genai.GenerativeModel(model_name='gemini-1.5-pro-latest', 
-                              system_instruction="""You are the helpful AI Assistant. Given a query you always helps with the relative query topic""")
+                              system_instruction="""You are the helpful AI Teaching Assistant. Given a Data Science topic you always helps the user uderstand the topic in simple way.If the question is not related to Data Science topic, response with 'This is beyond my knowledge' """)
 
-st.title('AI chatbot')
+st.title('AI Data Science Chatbot')
 
 if 'chat_history' not in st.session_state:
     st.session_state['chat_history']=[]
